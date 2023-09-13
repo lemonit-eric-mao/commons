3cfa37df7638539e40a8fe3576b489eaa3b5f0be
毛巳煜 <eric.mao@lemonit.cn>
2023-09-13 09:07:36 +0800

    add queryRow
    Signed-off-by: 毛巳煜 <eric.mao@lemonit.cn>

--- db/sqlite.go
+++ db/sqlite.go
@@ -22,6 +22,7 @@ func Close() error {
     return database.Close()
 }
 
+// Exec 执行增、删、改
 func Exec(query string, args ...any) (sql.Result, error) {
 
     // 语句无参数时，直接执行
@@ -38,6 +39,7 @@ func Exec(query string, args ...any) (sql.Result, error) {
     return stmt.Exec(args...)
 }
 
+// Query 查询多条
 func Query(query string, args ...any) (*sql.Rows, error) {
 
     // 准备语句的过程包括语法分析、编译以及优化
@@ -46,4 +48,13 @@ func Query(query string, args ...any) (*sql.Rows, error) {
         logger.Error(err)
     }
     return stmt.Query(args...)
+}
+
+// QueryRow 查询单条
+func QueryRow(query string, args ...any) *sql.Row {
+    stmt, err := database.Prepare(query)
+    if err != nil {
+        logger.Error(err)
+    }
+    return stmt.QueryRow(args...)
 }
